7b9c27d9df1695865e07ec5c9febadde107e41ee
kxs1119 <lifeisastage101@outlook.com>
2024-10-06 17:37:53 -0400

    added changes of post response to the website

--- src/send_data.py
+++ src/send_data.py
@@ -7,6 +7,7 @@ def send_data():
     Sends deer detection data to the backend server using a POST request.
     """
     # Data payload (replace latitude and longitude with actual data)
+    url = 'http://192.168.250.144:8080/api/deer-detection'
     data = {
         "deer_detected": True,
         "location": {
@@ -15,10 +16,9 @@ def send_data():
         },
         "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     }
-
     # Send the data to the backend API endpoint
     try:
-        response = requests.post('http://127.0.0.1:5000/api/deer-detection', json=data)
+        response = requests.post(url, json=data)
         response.raise_for_status()  # Raises an HTTPError for bad responses
         print("Data sent successfully!")
     except requests.exceptions.HTTPError as err:
