5f9ee512031ae34fbdbbf62b83cf6254edb1e033
Kenny Smith <122045137+kxs1119@users.noreply.github.com>
2024-11-13 15:16:33 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://kxs1119:abc@100.83.20.6/deersafe')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://kxs1119:abc@100.113.233.66/deersafe')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
